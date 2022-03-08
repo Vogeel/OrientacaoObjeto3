5eda5a148bab8c74b7c7a67b610b450586dafc4f
Vogeel <vitorvogel12@gmail.com>
2022-03-08 15:08:59 -0300

    Add project files.

---
+++ OrientacaoObjeto3.ConsoleApp/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrientacaoObjeto3.ConsoleApp
+{
+    internal class Cilindro
+    {
+        public double altura;
+        public double raio;
+        public double Volume()
+        {
+            double ValorFInal = 3.14 * raio * raio * altura;
+            return ValorFInal;
+        }
+    }
+}
---
+++ OrientacaoObjeto3.ConsoleApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace OrientacaoObjeto3.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Cilindro CalculoCilindro = new Cilindro();
+
+            CalculoCilindro.altura = 5;
+            CalculoCilindro.raio = 2.5;
+            Console.WriteLine($"O volume do cilindro é de {CalculoCilindro.Volume():F2}");
+            Console.ReadKey();
+        }
+    }
+}
